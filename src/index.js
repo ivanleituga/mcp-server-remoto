e77a5e1152b935afb7cf69a44154e7b259033630
ivanleituga <ivan_leituga@hotmail.com>
2025-07-18 17:10:49 -0300

    port adjustment

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432
+  port: process.env.DB_PORT
 });
 
 // Schema do banco (você vai colar aqui)
